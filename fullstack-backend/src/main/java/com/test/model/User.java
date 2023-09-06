7decaa2cd82aec7fbe1246e33459b246529f77df
amishaj-29 <121154003+amishaj-29@users.noreply.github.com>
2023-09-06 10:06:19 +0530

    second commit

---
+++ fullstack-backend/src/main/java/com/test/model/User.java
@@ -0,0 +1,73 @@
+package com.test.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class User
+{
+	@Id
+	@GeneratedValue
+	private long id;
+
+	private String name;
+	private String lname;
+	private String address;
+	private String city;
+	private String state;
+	private String email;
+	private String phone;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	
+
+}
